57455b3add54d930b4db119d8ecf553992efb328
Akash Yadav <ashu2612yadav@gmail.com>
2023-05-06 23:18:03 +0530

    .env mongo cloud

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use("/api/flights", FlightRoute);
 app.use('/api/flight/bookings', flightBooking);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
