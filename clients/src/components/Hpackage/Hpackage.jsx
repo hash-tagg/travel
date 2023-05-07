950356e45a268556f62324ef0a1a357d0f337ec7
Akash Yadav <ashu2612yadav@gmail.com>
2023-05-07 21:05:18 +0530

    updated final code

--- clients/src/components/Hpackage/Hpackage.jsx
+++ clients/src/components/Hpackage/Hpackage.jsx
@@ -79,7 +79,8 @@ const HolidayPackages = () => {
   }
   return (
     <Layout>
-      <div className="bg-gray-100 py-8">
+      <div className="bg-gray-100 ">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="container mx-auto px-4">
           <h1 className="text-4xl font-bold mb-8">Holiday Packages</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
@@ -112,6 +113,7 @@ const HolidayPackages = () => {
             ))}
           </div>
         </div>
+      </div>
       </div>
     </Layout>
   );
